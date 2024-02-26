f856fba166976dc4cd88459e83765104ce2eec31
huseyn1951 <sultanlkhuseyn@gmail.com>
2024-02-26 20:32:10 +0400

    les19

---
+++ ConsoleApp25/MonthExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    public static class MonthExtension
+    {
+        public static string FindSeason(this Months month)
+        {
+            switch (month)
+            {
+                case Months.Dekabr:
+                case Months.Yanvar:       
+                case Months.Fevral:
+                    return "winter";
+
+
+                case Months.Mart:        
+                case Months.Aprel:      
+                case Months.May:
+                 return "spring";
+
+                case Months.İyun: 
+                case Months.İyul:
+                case Months.Avqust:
+                    return "summer";
+
+
+                case Months.Sentyabr: 
+                case Months.Oktyabr:
+                case Months.Noyabr:
+                    return "autumn";
+                    
+                default:return "incorrect month";
+                    break;
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Months month1 = Months.Dekabr;
+            Console.WriteLine($"{month1} is {month1.FindSeason()} season");
+
+            Months month2 = Months.Oktyabr;
+            Console.WriteLine($"{month2} is {month2.FindSeason()} season");
+
+        }
+    }
+}
